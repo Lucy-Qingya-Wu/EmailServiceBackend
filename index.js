ea3179e7e7b0e82ce1579e56330d250ce53b9575
Lucy-Qingya-Wu <lucy.wu000@gmail.com>
2018-04-02 12:24:34 -0400

    set up a backend server using express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// this app object here is used to set up configuration that will listen for incoming requests
+const app = express() // express() generates a new applicaiton that represents a running express app
+
+
+app.get('/', (req, res)=>{
+	console.log("req: ", req)
+	res.send({hi:'there'});
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT)
