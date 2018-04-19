0c40f7b2062087e63f579942182b3d7ff9d341c7
Lucy-Qingya-Wu <lucy.wu000@gmail.com>
2018-04-18 21:17:51 -0400

    test - push code to heroku

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//keys.js - figure out what set of credentials to return
+
+//Node.js applications now default to NODE_ENV=production
+if (process.env.NODE_ENV === 'production'){
+	module.exports = require('./prod');
+}else{
+	module.exports = require('./dev');
+}
